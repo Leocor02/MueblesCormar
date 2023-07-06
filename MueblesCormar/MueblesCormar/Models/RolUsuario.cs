d7eaf1936bf089359bc894c6ea2361c93fa75fd3
Leonardo Cortes <cortesleo02@gmail.com>
2023-07-06 17:44:30 -0600

    Login y registro

---
+++ MueblesCormar/MueblesCormar/Models/RolUsuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MueblesCormar.Models
+{
+    public class RolUsuario
+    {
+        public RolUsuario()
+        {
+            Usuarios = new HashSet<Usuario>();
+        }
+
+        public int IdrolUsuario { get; set; }
+        public string Rol { get; set; } = null!;
+
+        public virtual ICollection<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ MueblesCormar/MueblesCormar/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MueblesCormar.Models
+{
+    public class Usuario
+    {
+        public Usuario()
+        {
+            //Bitacoras = new HashSet<Bitacora>();
+            //Registros = new HashSet<Registro>();
+        }
+
+        public int Idusuario { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Contraseña { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public int IdrolUsuario { get; set; }
+
+        public virtual RolUsuario IdrolUsuarioNavigation { get; set; } = null!;
+
+        //public virtual ICollection<Bitacora> Bitacoras { get; set; }
+        //public virtual ICollection<Registro> Registros { get; set; }
+    }
+}
