ace91aaf3b2843c595f835cd50265c6ede98f788
Leonardo Cortes <cortesleo02@gmail.com>
2023-07-08 14:02:29 -0600

    Diseño del UserSignUpPage

--- MueblesCormar/MueblesCormar/Services/CnnToAPI.cs
+++ MueblesCormar/MueblesCormar/Services/CnnToAPI.cs
@@ -9,8 +9,8 @@ namespace MueblesCormar.Services
         //Aquí se definen las rutas de consumo del API
         //Además se define la info de Api key necesaria para consumir los controladores
 
-        public static string ProductionURL = "http://192.168.0.3:45455/api/";
-        public static string TestingRoute = "http://192.168.0.3:45455/api/";
+        public static string ProductionURL = "http://192.168.0.9:45455/api/";
+        public static string TestingRoute = "http://192.168.0.9:45455/api/";
 
         public static string ApiKeyName = "MCApiKey";
         public static string ApiKeyValue = "MCinventario123";
